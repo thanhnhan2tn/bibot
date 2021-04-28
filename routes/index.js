961d2aaa6308cdee0f717e695861d453555a9133
arc_nhan.thai <nhan.thai@sutrixsolutions.com>
2021-04-28 14:41:35 +0700

    UD

--- routes/index.js
+++ routes/index.js
@@ -50,12 +50,37 @@ router.get('/', function (req, res, next) {
 				console.log(error.code);  // => 'ETELEGRAM'
 				console.log(error.response.body); // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
 			});
+
 		} catch (error) {
 			console.log(error);
 		}
 	}
+
+
 	try {
 		binance.websockets.userData(function () { console.log('1'); }, execution_update);
+		bot.onText(/\/echo (.+)/, (msg, match) => {
+			// 'msg' is the received Message from Telegram
+			// 'match' is the result of executing the regexp above on the text content
+			// of the message
+		
+			const chatId = msg.chat.id;
+			const resp = match[1]; // the captured "whatever"
+		
+			// send back the matched "whatever" to the chat
+			bot.sendMessage(chatId, resp);
+		});
+		
+		// Listen for any kind of message. There are different kinds of
+		// messages.
+		bot.on('message', (msg) => {
+			const chatId = msg.chat.id;
+		
+			// send a message to the chat acknowledging receipt of their message
+			bot.sendMessage(chatId, 'Received your message');
+		});
+		
+		res.render('connected');
 	} catch (e) {
 		console.log(e);
 	}
